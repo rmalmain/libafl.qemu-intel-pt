9c7d524b61157ac1037131c096f9331af32b525c
Marco C. <cavenati.marco@gmail.com>
2024-08-26 16:02:38 +0200

    Add link to Matin Splitt's article

--- test_intel_pt/boot.s
+++ test_intel_pt/boot.s
@@ -1,3 +1,5 @@
+; Inspired by Martin Splitt's article https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf
+
 [bits 16] ; use 16 bits 
 [org 0x7c00] ; sets the start address 
 
